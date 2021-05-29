e65265634787258c512d8c980448f046f87d3c48
Stuti08coder <76516771+Stuti08coder@users.noreply.github.com>
2021-05-29 15:09:50 +0530

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,23 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.4,
+            length: 10
+        }
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher)
+    }
+}
+    
+
+display(){
+    if(this.launcher.bodyA){
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
